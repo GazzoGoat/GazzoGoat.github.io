4c76eda3566309bb54d0f12a95e49f9b8cd641ec
GazzoGoat <141849768+GazzoGoat@users.noreply.github.com>
2024-02-13 01:09:30 +0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,22 @@
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function(){
+    header.classList.toggle ("sticky", this.window.scrollY > 0);
+})
+
+
+let menu = document.querySelector('#menu-icon');
+let navmenu = document.querySelector('.navmenu');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navmenu.classList.toggle('open')
+}
+
+const navigation = document.querySelector(".primary-navigation");
+const navigationHeight = navigation.offsetHeight;
+
+document.documentElement.style.setProperty(
+    "--scroll-padding",
+    headerheight + "px"
+);
